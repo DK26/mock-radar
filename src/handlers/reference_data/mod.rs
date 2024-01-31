cd855a2f9d17c954da739713dfdb93d18c02de56
David Krasnitsky <dikaveman@gmail.com>
2024-02-01 00:09:20 +0200

    Fully implemented the test `post_reference_set_with_token_success()`

--- src/handlers/reference_data/mod.rs
+++ src/handlers/reference_data/mod.rs
@@ -6,7 +6,7 @@ pub(crate) mod sets;
 
 pub(crate) fn create_routes() -> Router<SharedQRadarMock> {
     Router::new().nest(
-        "/reference_data",
+        "/api/reference_data",
         Router::new()
             .route("/sets", axum::routing::get(sets::get_sets_handler))
             .route("/sets", axum::routing::post(sets::post_sets_handler)),
--- tests/reference_set.rs
+++ tests/reference_set.rs
@@ -1,13 +1,36 @@
+mod common;
+
+use axum::body::to_bytes;
 use axum::{
     body::Body,
     http::{self, Request, StatusCode},
 };
-use mock_radar::SharedQRadarMock;
+use mock_radar::{SharedQRadarMock, REGISTERED_TOKEN};
+use serde::Deserialize;
 use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
 
-pub const REGISTERED_TOKEN: &str = "d6391576-55d3-4c44-85d8-5665b0d2336f";
+const ENDPOINT_URI: &str = "/api/reference_data/sets";
 
-const ENDPOINT_REFERENCE_SET: &str = "/reference_data/sets";
+// TODO: Move types to a module of their own
+#[derive(Debug, Deserialize)]
+struct TestGetResponse {
+    timeout_type: String,
+    number_of_elements: u32,
+    #[allow(unused)]
+    creation_time: u64, // Assuming successful deserialization implies a valid timestamp
+    name: String,
+    element_type: String,
+}
+
+impl PartialEq for TestGetResponse {
+    fn eq(&self, other: &Self) -> bool {
+        self.timeout_type == other.timeout_type
+            && self.number_of_elements == other.number_of_elements
+            && self.name == other.name
+            && self.element_type == other.element_type
+        // Ignoring `creation_time` in comparison
+    }
+}
 
 #[tokio::test]
 pub(crate) async fn get_reference_set() {
@@ -19,7 +42,7 @@ pub(crate) async fn get_reference_set() {
         .oneshot(
             Request::builder()
                 .method(http::Method::GET)
-                .uri(ENDPOINT_REFERENCE_SET)
+                .uri(ENDPOINT_URI)
                 .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
                 .body(Body::empty())
                 .unwrap(),
@@ -31,14 +54,14 @@ pub(crate) async fn get_reference_set() {
 }
 
 #[tokio::test]
-pub(crate) async fn post_reference_set() {
+pub(crate) async fn post_reference_set_with_token_success() {
     let shared_qradar_mock = SharedQRadarMock::default();
     let router = mock_radar::create_routes();
 
     let name: &str = "test_ip_addresses";
     let element_type = "IP";
 
-    let uri = format!("{ENDPOINT_REFERENCE_SET}?element_type={element_type}&name={name}");
+    let uri = format!("{ENDPOINT_URI}?element_type={element_type}&name={name}");
 
     let response = router
         .with_state(shared_qradar_mock)
@@ -49,10 +72,7 @@ pub(crate) async fn post_reference_set() {
                 .header("Version", "12.0")
                 .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
                 .header(http::header::ACCEPT, mime::APPLICATION_JSON.as_ref())
-                .header(
-                    http::header::AUTHORIZATION,
-                    format!("Bearer {REGISTERED_TOKEN}"),
-                )
+                .header("SEC", REGISTERED_TOKEN)
                 .body(Body::empty())
                 .unwrap(),
         )
@@ -61,14 +81,32 @@ pub(crate) async fn post_reference_set() {
 
     assert_eq!(response.status(), StatusCode::CREATED);
 
-    /*
-    RESPONSE BODY:
-        {
-            "timeout_type": "UNKNOWN",
-            "number_of_elements": 0,
-            "creation_time": 1706655294903,
-            "name": "test1234",
-            "element_type": "IP"
+    let body = to_bytes(response.into_body(), usize::MAX).await.unwrap();
+
+    let body: TestGetResponse = serde_json::from_slice(&body).unwrap();
+
+    assert_eq!(
+        body,
+        TestGetResponse {
+            timeout_type: "UNKNOWN".to_string(),
+            number_of_elements: 0,
+            creation_time: 0, // Ignoring `creation_time` in comparison
+            name: name.to_string(),
+            element_type: element_type.to_string()
         }
-    */
+    )
+}
+
+pub(crate) async fn post_reference_set_with_again_with_token_failure() {
+    // {
+    //     "http_response": {
+    //       "code": 409,
+    //       "message": "The request could not be completed due to a conflict with the current state of the resource"
+    //     },
+    //     "code": 1004,
+    //     "description": "The reference set could not be created, the name provided is already in use. Please change the name and try again.",
+    //     "details": {},
+    //     "message": "The name test1234 is already in use"
+    //   }
+    todo!()
 }
