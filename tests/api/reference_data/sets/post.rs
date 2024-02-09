1d7e0fcc150eb36d6f68dba373733b26c03d6153
David Krasnitsky <dikaveman@gmail.com>
2024-02-10 00:06:37 +0200

    Implemented the `post_reference_set_with_readonly_sec_token_forbidden_failure()` integration test

--- tests/api/reference_data/sets/post.rs
+++ tests/api/reference_data/sets/post.rs
@@ -6,7 +6,7 @@ use serde::Deserialize;
 use serde_json::json;
 use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
 
-use mock_radar::{SharedQRadarMock, REGISTERED_SEC_TOKEN};
+use mock_radar::{SharedQRadarMock, REGISTERED_READONLY_SEC_TOKEN, REGISTERED_SEC_TOKEN};
 
 use super::ENDPOINT_URI;
 
@@ -216,6 +216,59 @@ pub(crate) async fn post_reference_set_with_sec_token_invalid_element_type_failu
                 "description": "",
                 "details": {},
                 "message": "Failed to transform user query parameter \"element_type\" with a content type of \"TEXT_PLAIN\""
+            }
+        )
+    );
+}
+
+#[tokio::test]
+pub(crate) async fn post_reference_set_with_readonly_sec_token_forbidden_failure() {
+    let shared_qradar_mock = SharedQRadarMock::default();
+    let router = mock_radar::create_routes();
+
+    let name: &str = "test_ip_addresses";
+    let element_type = "IP";
+
+    let uri = format!("{ENDPOINT_URI}?element_type={element_type}&name={name}");
+    let encoded_uri = format!("/{}", urlencoding::encode(&uri));
+
+    let response = router
+        .clone()
+        .with_state(shared_qradar_mock.clone())
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri(&encoded_uri)
+                .header("Version", "12.0")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .header(http::header::ACCEPT, mime::APPLICATION_JSON.as_ref())
+                .header("SEC", REGISTERED_READONLY_SEC_TOKEN)
+                .body(Body::empty())
+                .expect("could not build request"),
+        )
+        .await
+        .expect("could not get response");
+
+    assert_eq!(response.status(), StatusCode::FORBIDDEN);
+
+    let response_body_bytes = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .expect("cannot convert response to Bytes");
+    let response_body: serde_json::Value = serde_json::from_slice(&response_body_bytes)
+        .expect("cannot deserialize response from Bytes");
+
+    assert_eq!(
+        response_body,
+        json!(
+            {
+                "http_response": {
+                    "code": 403,
+                    "message": "Your account is not authorized to access the requested resource"
+                },
+                "code": 26,
+                "description": "",
+                "details": {},
+                "message": "User has insufficient capabilities to access this endpoint resource"
             }
         )
     );
