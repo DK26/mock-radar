828276b33c7d130b9e776259f461ac2923129359
David Krasnitsky <dikaveman@gmail.com>
2024-02-18 23:41:25 +0200

    Updated the error sequence test for `POST /api/reference_data/sets`

--- tests/api/reference_data/sets/post/post_reference_set_with_sec_token_request_invalidation_sequence_failure.rs
+++ tests/api/reference_data/sets/post/post_reference_set_with_sec_token_request_invalidation_sequence_failure.rs
@@ -17,23 +17,10 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
 
     // Sequence 1
 
-    // Mandatory fields
-    let element_type = "invalid_element";
-    let name = "test_ip_addresses";
+    // Optional fields (default = selects all fields)
+    let fields = "invalid";
 
-    // Optional fields
-    let fields = "non_existent";
-    let time_to_live = "invalid time";
-    let timeout_type = "INVALID_TYPE";
-
-    let uri = format!(
-        "{ENDPOINT_URI}?element_type={}&name={}&fields={}&time_to_live={}&timeout_type={}",
-        urlencoding::encode(element_type),
-        urlencoding::encode(name),
-        urlencoding::encode(fields),
-        urlencoding::encode(time_to_live),
-        urlencoding::encode(timeout_type),
-    );
+    let uri = format!("{ENDPOINT_URI}?fields={}", urlencoding::encode(fields));
 
     let response = router
         .clone()
@@ -65,36 +52,23 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
         json!(
             {
                 "http_response": {
-                  "code": 422,
-                  "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
                 },
                 "code": 30,
                 "description": "",
                 "details": {},
                 "message": format!("Specified field \"{fields}\" is not recognized by this endpoint for media type \"application/json\"")
-              }
+            }
         )
     );
 
     // Sequence 2
 
-    // Mandatory fields
-    let element_type = "invalid_element";
-    let name = "test_ip_addresses";
+    // `name` is a field that exists, but not yet provided
+    let fields = "name";
 
-    // Optional fields
-    let fields = "time_to_live";
-    let time_to_live = "invalid time";
-    let timeout_type = "INVALID_TYPE";
-
-    let uri = format!(
-        "{ENDPOINT_URI}?element_type={}&name={}&fields={}&time_to_live={}&timeout_type={}",
-        urlencoding::encode(element_type),
-        urlencoding::encode(name),
-        urlencoding::encode(fields),
-        urlencoding::encode(time_to_live),
-        urlencoding::encode(timeout_type),
-    );
+    let uri = format!("{ENDPOINT_URI}?fields={}", urlencoding::encode(fields));
 
     let response = router
         .clone()
@@ -125,14 +99,14 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
         response_body,
         json!(
             {
-            "http_response": {
-                "code": 422,
-                "message": "The request was well-formed but was unable to be followed due to semantic errors"
-            },
-            "code": 11,
-            "description": "",
-            "details": {},
-            "message": "Failed to transform user query parameter \"element_type\" with a content type of \"TEXT_PLAIN\""
+                "http_response": {
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                },
+                "code": 8,
+                "description": "",
+                "details": {},
+                "message": "Missing required parameter \"name\" from query parameters"
             }
         )
     );
@@ -140,21 +114,15 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
     // Sequence 3
 
     // Mandatory fields
-    let element_type = "IP";
-    let name = "test_ip_addresses";
+    let name = "some_name";
 
     // Optional fields
-    let fields = "time_to_live";
-    let time_to_live = "invalid time";
-    let timeout_type = "INVALID_TYPE";
+    let fields = "name";
 
     let uri = format!(
-        "{ENDPOINT_URI}?element_type={}&name={}&fields={}&time_to_live={}&timeout_type={}",
-        urlencoding::encode(element_type),
-        urlencoding::encode(name),
+        "{ENDPOINT_URI}?fields={}&name={}",
         urlencoding::encode(fields),
-        urlencoding::encode(time_to_live),
-        urlencoding::encode(timeout_type),
+        urlencoding::encode(name),
     );
 
     let response = router
@@ -187,34 +155,89 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
         json!(
             {
                 "http_response": {
-                  "code": 422,
-                  "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
                 },
-                "code": 11,
+                "code": 8,
                 "description": "",
                 "details": {},
-                "message": "Failed to transform user query parameter \"timeout_type\" with a content type of \"TEXT_PLAIN\""
-              }
+                "message": "Missing required parameter \"element_type\" from query parameters"
+            }
         )
     );
 
     // Sequence 4
 
     // Mandatory fields
-    let element_type = "IP";
-    let name = "test_ip_addresses";
+    let name = "some_name";
+    let element_type = "invalid";
 
     // Optional fields
-    let fields = "time_to_live";
-    let time_to_live = "invalid time";
-    let timeout_type = "LAST_SEEN";
+    let fields = "name";
 
     let uri = format!(
-        "{ENDPOINT_URI}?element_type={}&name={}&fields={}&time_to_live={}&timeout_type={}",
-        urlencoding::encode(element_type),
-        urlencoding::encode(name),
+        "{ENDPOINT_URI}?fields={}&name={}&element_type={}",
         urlencoding::encode(fields),
-        urlencoding::encode(time_to_live),
+        urlencoding::encode(name),
+        urlencoding::encode(element_type),
+    );
+
+    let response = router
+        .clone()
+        .with_state(shared_qradar_mock.clone())
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri(&uri)
+                .header("Version", "12.0")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .header(http::header::ACCEPT, mime::APPLICATION_JSON.as_ref())
+                .header("SEC", REGISTERED_SEC_TOKEN)
+                .body(Body::empty())
+                .expect("could not build request"),
+        )
+        .await
+        .expect("could not get response");
+
+    assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+    let response_body_bytes = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .expect("cannot convert response to Bytes");
+    let response_body: serde_json::Value = serde_json::from_slice(&response_body_bytes)
+        .expect("cannot deserialize response from Bytes");
+
+    assert_eq!(
+        response_body,
+        json!(
+            {
+                "http_response": {
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                },
+                "code": 11,
+                "description": "",
+                "details": {},
+                "message": "Failed to transform user query parameter \"element_type\" with a content type of \"TEXT_PLAIN\""
+            }
+        )
+    );
+
+    // Sequence 5
+
+    // Mandatory fields
+    let element_type = "IP";
+    let name = "some_name";
+
+    // Optional fields
+    let fields = "name";
+    let timeout_type = "invalid";
+
+    let uri = format!(
+        "{ENDPOINT_URI}?fields={}&name={}&element_type={}&timeout_type={}",
+        urlencoding::encode(fields),
+        urlencoding::encode(name),
+        urlencoding::encode(element_type),
         urlencoding::encode(timeout_type),
     );
 
@@ -248,8 +271,69 @@ pub(crate) async fn post_reference_set_with_sec_token_request_invalidation_seque
         json!(
             {
                 "http_response": {
-                  "code": 422,
-                  "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
+                },
+                "code": 11,
+                "description": "",
+                "details": {},
+                "message": "Failed to transform user query parameter \"timeout_type\" with a content type of \"TEXT_PLAIN\""
+            }
+        )
+    );
+
+    // Sequence 6
+
+    // Mandatory fields
+    let element_type = "IP";
+    let name = "some_name";
+
+    // Optional fields
+    let fields = "name";
+    let timeout_type = "LAST_SEEN";
+    let time_to_live = "invalid";
+
+    let uri = format!(
+        "{ENDPOINT_URI}?fields={}&name={}&element_type={}&timeout_type={}&time_to_live={}",
+        urlencoding::encode(fields),
+        urlencoding::encode(name),
+        urlencoding::encode(element_type),
+        urlencoding::encode(timeout_type),
+        urlencoding::encode(time_to_live),
+    );
+
+    let response = router
+        .clone()
+        .with_state(shared_qradar_mock.clone())
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri(&uri)
+                .header("Version", "12.0")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .header(http::header::ACCEPT, mime::APPLICATION_JSON.as_ref())
+                .header("SEC", REGISTERED_SEC_TOKEN)
+                .body(Body::empty())
+                .expect("could not build request"),
+        )
+        .await
+        .expect("could not get response");
+
+    assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+    let response_body_bytes = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .expect("cannot convert response to Bytes");
+    let response_body: serde_json::Value = serde_json::from_slice(&response_body_bytes)
+        .expect("cannot deserialize response from Bytes");
+
+    assert_eq!(
+        response_body,
+        json!(
+            {
+                "http_response": {
+                    "code": 422,
+                    "message": "The request was well-formed but was unable to be followed due to semantic errors"
                 },
                 "code": 1005,
                 "description": "A request parameter is not valid",
